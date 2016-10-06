da4e25217f893db97d7260cce14b6cf9af7c6b32
Dimitris-Theodoropoulos <Theodoropoulos.Dimitris2@gmail.com>
2016-10-07 00:29:50 +0300

    Sql day 1 exercise 1

---
+++ sql_exercise_1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `bootcamp`.`courses` (
+  `course id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `name of the lecturer` VARCHAR(45) NULL,
+  `name of the assistant` VARCHAR(45) NULL,
+  `duration of the course` INT NULL,
+  `starting date of the course` DATE NULL,
+  `coursescol` VARCHAR(45) NULL,
+  PRIMARY KEY (`course id`));
