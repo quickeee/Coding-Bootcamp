c628ce213eee4343372467e1087c2a8dd4bbff50
Dimitris-Theodoropoulos <Theodoropoulos.Dimitris2@gmail.com>
2016-10-07 04:28:25 +0300

    Delete sql_exercise_4.sql

--- sql_exercise_4.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO bootcamp.courses
-VALUES (1, "SQL", "AG", "BN", 2, '2016-10-21'),
-	   (2, "Java", "VS", "BN", 3, '2016-10-25'),
-       (3, "R", "KP", "TT", 4, '2016-11-09'),
-       (4, "Python", "KK", "DD", 5, '2016-11-09');
